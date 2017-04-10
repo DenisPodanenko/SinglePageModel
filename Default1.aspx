8e0eb094f719bb750f115e2684abd88c0f92b380
DenisPodanenko <d.podanenko@gmail.com>
2017-04-10 18:55:25 +0300

    first commit

---
+++ Default1.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#"  Debug="true"%>
+
+<%@ Import Namespace="AutoLotConnectedLayer" %>
+<%@ Assembly Src="AutoLotDAL.cs" %>
+
+<!DOCTYPE html>
+<script runat="server">
+    protected void btnFillData_click(object sender, EventArgs e)
+    {
+        InventoryDAL dal = new InventoryDAL();
+        dal.OpenConnection(@"Data Source=comp\SQLEXPRESS;Initial Catalog=AutoLot;Integrated Security=True");
+        carsGridView.DataSource = dal.GetAllInventoryAsList();
+        carsGridView.DataBind();
+        dal.CloseConnection();
+    }
+</script>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta charset="utf-8" />
+    <title></title>    
+    <style type="text/css">
+        #form1 {
+            height: 256px;
+            width: 696px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">   
+        <asp:Label ID="lblInfo" runat="server" Text="Click on the Button to Fill the Grid"></asp:Label>
+        <br />
+        <br />
+        <asp:GridView ID="carsGridView" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+            <AlternatingRowStyle BackColor="White" />
+            <EditRowStyle BackColor="#7C6F57" />
+            <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#E3EAEB" />
+            <SelectedRowStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F8FAFA" />
+            <SortedAscendingHeaderStyle BackColor="#246B61" />
+            <SortedDescendingCellStyle BackColor="#D4DFE1" />
+            <SortedDescendingHeaderStyle BackColor="#15524A" />
+        </asp:GridView>
+        <br />
+        <asp:Button ID="btnFillData" runat="server" Height="40px" OnClick="btnFillData_click" style="margin:5px" Text="Fill Grid" Width="89px" />
+    </form>
+</body>
+</html>
---
+++ bin/AutoLotDAL.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AutoLotConnectedLayer
+{
+    public class InventoryDAL
+    {
+        private SqlConnection sqlCn = null;
+
+        public void OpenConnection(string connectionString)
+        {
+            sqlCn = new SqlConnection();
+            sqlCn.ConnectionString = connectionString;
+            sqlCn.Open();
+        }
+
+        public void CloseConnection()
+        {
+            sqlCn.Close();
+        }
+
+        public void InsertAuto(int id, string color, string make, string petName)
+        {
+            string sql = string.Format("Insert Into Inventory" + "(CarID, Make, Color, PetName) Values" + "(@CarID, @Make, @Color, @PetName)");
+
+            using (SqlCommand cmd = new SqlCommand(sql, this.sqlCn))
+            {
+                SqlParameter param = new SqlParameter();
+                param.ParameterName = "@CarID";
+                param.Value = id;
+                param.SqlDbType = SqlDbType.Int;
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter();
+                param.ParameterName = "@Make";
+                param.Value = make;
+                param.SqlDbType = SqlDbType.Char;
+                param.Size = 10;
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter();
+                param.ParameterName = "@Color";
+                param.Value = color;
+                param.SqlDbType = SqlDbType.Char;
+                param.Size = 10;
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter();
+                param.ParameterName = "@petName";
+                param.Value = petName;
+                param.SqlDbType = SqlDbType.Char;
+                param.Size = 10;
+                cmd.Parameters.Add(param);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void InsertAuto(NewCar car)
+        {
+            string sql = string.Format("Insert Into Inventory" + "(CarID, Make, Color, PetName) Values" + "('{0}','{1}','{2}','{3}')", car.CarID, car.Make, car.Color, car.PetName);
+
+            using (SqlCommand cmd = new SqlCommand(sql, this.sqlCn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteCar(int id)
+        {
+            string sql = string.Format("DELETE FROM Inventory where CarID = '{0}'", id);
+
+            using (SqlCommand cmd = new SqlCommand(sql, this.sqlCn))
+            {
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    Exception error = new Exception("Sorry! That car is on order!", ex);
+                    throw error;
+                }
+            }
+        }
+
+        public void UpdateCarPetName(int id, string newPetName)
+        {
+            string sql = string.Format("UPDATE Inventory SET PetName = '{0}' WHERE CarID = '{1}'", newPetName, id);
+
+            using (SqlCommand cmd = new SqlCommand(sql, this.sqlCn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public List<NewCar> GetAllInventoryAsList()
+        {
+            List<NewCar> inv = new List<NewCar>();
+
+            string sql = "SELECT * FROM Inventory";
+            using (SqlCommand cmd = new SqlCommand(sql, this.sqlCn))
+            {
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    inv.Add(new NewCar
+                    {
+                        CarID = (int)dr["CarID"],
+                        Color = (string)dr["Color"],
+                        Make = (string)dr["Make"],
+                        PetName = (string)dr["PetName"]
+                    });
+                }
+                dr.Close();
+            }
+            return inv;
+        }
+
+        public DataTable GetAllinventoryAsDataTable()
+        {
+            DataTable inv = new DataTable();
+
+            string sql = "SELECT * FROM Inventory";
+            using (SqlCommand cmd = new SqlCommand(sql, this.sqlCn))
+            {
+                SqlDataReader dr = cmd.ExecuteReader();
+                inv.Load(dr);
+                dr.Close();
+            }
+            return inv;
+        }
+
+        public string LookUpPetName(int CarID)
+        {
+            string carPetName = string.Empty;
+
+            using (SqlCommand cmd = new SqlCommand("GetPetName", this.sqlCn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter param = new SqlParameter();
+                param.ParameterName = "@CarID";
+                param.SqlDbType = SqlDbType.Int;
+                param.Value = CarID;
+
+                param.Direction = ParameterDirection.Input;
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter();
+                param.ParameterName = "@petName";
+                param.SqlDbType = SqlDbType.Char;
+                param.Size = 10;
+                param.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(param);
+
+                cmd.ExecuteNonQuery();
+
+                carPetName = (string)cmd.Parameters["@petName"].Value;
+            }
+            return carPetName;
+        }
+    }
+
+    public class NewCar
+    {
+        public int CarID { get; set; }
+        public string Color { get; set; }
+        public string Make { get; set; }
+        public string PetName { get; set; }
+    }
+}
